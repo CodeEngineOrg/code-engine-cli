980e6bf443704f290d6b3ce068bf8b58f3ecddea
James Messinger <james.messinger@gmail.com>
2019-11-27 07:37:30 -0600

    Updated the exports tests now that the package exports a class instead of a function

--- test/specs/exports.spec.js
+++ test/specs/exports.spec.js
@@ -1,22 +1,22 @@
 "use strict";
 
 const commonJSExport = require("../../");
-const { default: defaultExport, main: namedExport } = require("../../");
+const { default: defaultExport, CodeEngineCLI: namedExport } = require("../../");
 const { expect } = require("chai");
 
 describe("@code-engine/cli package exports", () => {
 
-  it("should export the main() function as the default CommonJS export", () => {
+  it("should export the CodeEngineCLI class as the default CommonJS export", () => {
     expect(commonJSExport).to.be.a("function");
-    expect(commonJSExport.name).to.equal("main");
+    expect(commonJSExport.name).to.equal("CodeEngineCLI");
   });
 
-  it("should export the main() function as the default ESM export", () => {
+  it("should export the CodeEngineCLI class as the default ESM export", () => {
     expect(defaultExport).to.be.a("function");
     expect(defaultExport).to.equal(commonJSExport);
   });
 
-  it("should export the main() function as a named export", () => {
+  it("should export the CodeEngineCLI class as a named export", () => {
     expect(namedExport).to.be.a("function");
     expect(namedExport).to.equal(commonJSExport);
   });
@@ -24,7 +24,7 @@ describe("@code-engine/cli package exports", () => {
   it("should not export anything else", () => {
     expect(commonJSExport).to.have.same.keys(
       "default",
-      "main",
+      "CodeEngineCLI",
     );
   });
 
