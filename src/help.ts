2bdf687ce69f2377dad2742d31080a143449202c
James Messinger <james.messinger@gmail.com>
2019-11-27 11:11:37 -0600

    Added ASCII art banner text

--- src/help.ts
+++ src/help.ts
@@ -1,6 +1,21 @@
 import { manifest } from "./manifest";
 
 /**
+ * Banner text displayed at startup
+ */
+export const bannerText =
+  " ______            __          ______                                 \n" +
+  "|   ___|          |  |        |   ___|               __               \n" +
+  "|  |     _____  __|  | _____  |  |_    __ __  _____ |__| __ __  _____ \n" +
+  "|  |    |  _  ||  _  ||  __ | |   _|  |  \\  ||  _  ||  ||  \\  ||  __ |\n" +
+  "|  |___ | |_| || |_| ||  __|  |  |___ |     || |_| ||  ||     ||  __| \n" +
+  "|______||_____||_____||_____| |______||__\\__||___  ||__||__\\__||_____|\n" +
+  "                                             |_____|                  \n" +
+  center(manifest.description, 70) + "\n" +
+  center(`v${manifest.version}`, 70) + "\n"
+;
+
+/**
  * Text explaining how to use the CLI
  */
 export const usageText = `
@@ -20,6 +35,14 @@ files...
 /**
  * Text describing the program and how to use it
  */
-export const helpText = `
-CodeEngine - ${manifest.description}
-${usageText}`;
+export const helpText = `\n${bannerText}${usageText}`;
+
+/**
+ * Centers a string
+ */
+function center(text: string, length: number): string {
+  let padding = Math.max(0, length - text.length);
+  let right = " ".repeat(Math.floor(padding / 2));
+  let left = " ".repeat(padding - right.length);
+  return `${left}${text}${right}`;
+}
