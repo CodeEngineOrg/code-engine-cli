95599b411609face484bf1560bf14a52f9b588e8
James Messinger <james.messinger@gmail.com>
2019-11-27 05:11:05 -0600

    Moved the error-handling logic out of the `parseArgs()` function (and into the `CodeEngineCLI` class)

--- src/parse-args.ts
+++ src/parse-args.ts
@@ -1,7 +1,4 @@
-// tslint:disable: no-console
 import * as commandLineArgs from "command-line-args";
-import { ExitCode } from "./exit-code";
-import { usageText } from "./help";
 
 /**
  * The parsed command-line arguments
@@ -16,30 +13,18 @@ export interface ParsedArgs {
  * Parses the command-line arguments
  */
 export function parseArgs(argv: string[]): ParsedArgs {
-  try {
-    let args = commandLineArgs(
-      [
-        { name: "quiet", alias: "q", type: Boolean },
-        { name: "version", alias: "v", type: Boolean },
-        { name: "help", alias: "h", type: Boolean },
-      ],
-      { argv }
-    );
+  let args = commandLineArgs(
+    [
+      { name: "quiet", alias: "q", type: Boolean },
+      { name: "version", alias: "v", type: Boolean },
+      { name: "help", alias: "h", type: Boolean },
+    ],
+    { argv }
+  );
 
-    return {
-      help: Boolean(args.help),
-      version: Boolean(args.version),
-      quiet: Boolean(args.quiet),
-    };
-  }
-  catch (error) {
-    // There was an error parsing the command-line args
-    return errorHandler(error as Error);
-  }
-}
-
-function errorHandler(error: Error): never {
-  console.error(error.message);
-  console.error(usageText);
-  return process.exit(ExitCode.InvalidArgument);
+  return {
+    help: Boolean(args.help),
+    version: Boolean(args.version),
+    quiet: Boolean(args.quiet),
+  };
 }
