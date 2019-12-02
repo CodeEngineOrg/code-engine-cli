c974c04ff59adbaa8005dd7f6fedd7d6470d4f12
James Messinger <james.messinger@gmail.com>
2019-12-02 05:25:17 -0600

    Exported the `Generator` interface

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { CodeEngineCLI } from "./cli";
 
 export { Config } from "./config";
+export * from "./generator";
 export { CodeEngineCLI };
 
 // Export `CodeEngineCLI` as the default export
