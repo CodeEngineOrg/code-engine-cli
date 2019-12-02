b3a093c6f17b74f16b761008f1675ff7946c2522
James Messinger <james.messinger@gmail.com>
2019-12-02 03:57:32 -0600

    Added tests for sources specified in the generator or on the command line

---
+++ test/specs/sources.spec.js
@@ -0,0 +1,182 @@
+"use strict";
+
+const CodeEngineCLI = require("../../lib");
+const MockProcess = require("../utils/process");
+const createDir = require("../utils/create-dir");
+const { expect } = require("chai");
+const { join } = require("path");
+
+describe("code-engine [sources...]", () => {
+
+  it("should default to all files in the current directory", async () => {
+    let dir = await createDir([
+      { path: "index.js", contents: "" },
+      { path: "file.txt", contents: "Hello, world!" },
+      { path: "subdir/index.html", contents: "<h1>Hello, World!</h1>" },
+      { path: "sub/dir/file.txt", contents: "Helloooooooo" },
+    ]);
+
+    let process = new MockProcess(dir);
+    let cli = new CodeEngineCLI({ process });
+    await cli.main();
+
+    process.assert.exitCode(0);
+    process.assert.stderr("");
+
+    expect(join(dir, "dist")).to.be.a.directory().with.deep.contents([
+      "index.js",
+      "file.txt",
+      "sub",
+      "sub/dir",
+      "sub/dir/file.txt",
+      "subdir",
+      "subdir/index.html",
+    ]);
+
+    expect(join(dir, "dist/index.js")).to.be.a.file().and.empty;
+    expect(join(dir, "dist/file.txt")).to.be.a.file().with.contents("Hello, world!");
+    expect(join(dir, "dist/subdir/index.html")).to.be.a.file().with.contents("<h1>Hello, World!</h1>");
+    expect(join(dir, "dist/sub/dir/file.txt")).to.be.a.file().with.contents("Helloooooooo");
+  });
+
+  it("should use the sources specified in the generator", async () => {
+    let dir = await createDir([
+      {
+        path: "index.js",
+        contents: `
+          module.exports = {
+            source: "**/*.txt",
+          }
+        `
+      },
+
+      { path: "file1.txt", contents: "Hello, world!" },
+      { path: "subdir/file2.html", contents: "<h1>Hello, World!</h1>" },
+      { path: "sub/dir/file3.txt", contents: "Helloooooooo" },
+    ]);
+
+    let process = new MockProcess(dir);
+    let cli = new CodeEngineCLI({ process });
+    await cli.main();
+
+    process.assert.exitCode(0);
+    process.assert.stderr("");
+
+    expect(join(dir, "dist")).to.be.a.directory().with.deep.contents([
+      "file1.txt",
+      "sub",
+      "sub/dir",
+      "sub/dir/file3.txt",
+    ]);
+
+    expect(join(dir, "dist/file1.txt")).to.be.a.file().with.contents("Hello, world!");
+    expect(join(dir, "dist/sub/dir/file3.txt")).to.be.a.file().with.contents("Helloooooooo");
+  });
+
+  it("should use the sources specified on the command line", async () => {
+    let dir = await createDir([
+      {
+        path: "index.js",
+        contents: `
+          module.exports = {
+            source: "**/*.txt",
+          }
+        `
+      },
+
+      { path: "file1.txt", contents: "Hello, world!" },
+      { path: "subdir/file2.html", contents: "<h1>Hello, World!</h1>" },
+      { path: "sub/dir/file3.txt", contents: "Helloooooooo" },
+      { path: "sub/dir/file4.html", contents: "<h1>Helloooooooo</h1>" },
+    ]);
+
+    let process = new MockProcess(dir);
+    let cli = new CodeEngineCLI({ process });
+    await cli.main([".", "**/*.html"]);
+
+    process.assert.exitCode(0);
+    process.assert.stderr("");
+
+    expect(join(dir, "dist")).to.be.a.directory().with.deep.contents([
+      "subdir",
+      "subdir/file2.html",
+      "sub",
+      "sub/dir",
+      "sub/dir/file4.html",
+    ]);
+
+    expect(join(dir, "dist/subdir/file2.html")).to.be.a.file().with.contents("<h1>Hello, World!</h1>");
+    expect(join(dir, "dist/sub/dir/file4.html")).to.be.a.file().with.contents("<h1>Helloooooooo</h1>");
+  });
+
+  it("should support multiple sources on the command line", async () => {
+    let dir = await createDir([
+      "index.js",
+      { path: "file1.txt", contents: "Hello, world!" },
+      { path: "subdir/file2.html", contents: "<h1>Hello, World!</h1>" },
+      { path: "sub/dir/file3.txt", contents: "Helloooooooo" },
+      { path: "sub/dir/file4.html", contents: "<h1>Helloooooooo</h1>" },
+    ]);
+
+    let process = new MockProcess(dir);
+    let cli = new CodeEngineCLI({ process });
+    await cli.main([".", "*.txt", "**/*2.*", "*/*/*.html"]);
+
+    process.assert.exitCode(0);
+    process.assert.stderr("");
+
+    expect(join(dir, "dist")).to.be.a.directory().with.deep.contents([
+      "file1.txt",
+      "subdir",
+      "subdir/file2.html",
+      "sub",
+      "sub/dir",
+      "sub/dir/file4.html",
+    ]);
+
+    expect(join(dir, "dist/file1.txt")).to.be.a.file().with.contents("Hello, world!");
+    expect(join(dir, "dist/subdir/file2.html")).to.be.a.file().with.contents("<h1>Hello, World!</h1>");
+    expect(join(dir, "dist/sub/dir/file4.html")).to.be.a.file().with.contents("<h1>Helloooooooo</h1>");
+  });
+
+  it("should error if a source specified in the generator doesn't exist", async () => {
+    let dir = await createDir([
+      {
+        path: "index.js",
+        contents: `
+          module.exports = {
+            source: "file.txt",
+          }
+        `
+      },
+    ]);
+
+    let process = new MockProcess(dir);
+    let cli = new CodeEngineCLI({ process });
+    await cli.main();
+
+    process.assert.exitCode(1);
+    process.assert.stderr(
+      "An error occurred in Filesystem Source while reading source files. \n" +
+      `ENOENT: no such file or directory, stat '${join(dir, "file.txt")}'\n`
+    );
+
+    expect(join(dir, "dist")).not.to.be.a.path();
+  });
+
+  it("should error if a source specified on the command-line doesn't exist", async () => {
+    let dir = await createDir(["index.js"]);
+    let process = new MockProcess(dir);
+    let cli = new CodeEngineCLI({ process });
+    await cli.main([".", "file.txt"]);
+
+    process.assert.exitCode(1);
+    process.assert.stderr(
+      "An error occurred in Filesystem Source while reading source files. \n" +
+      `ENOENT: no such file or directory, stat '${join(dir, "file.txt")}'\n`
+    );
+
+    expect(join(dir, "dist")).not.to.be.a.path();
+  });
+
+});
