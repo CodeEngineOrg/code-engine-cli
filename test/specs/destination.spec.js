481ea7ea1ab8c6c81ce52f322e177d35d2c8e79f
James Messinger <james.messinger@gmail.com>
2019-12-15 09:47:28 -0600

    Added tests to ensure that the output directory is cleaned before building

--- test/specs/destination.spec.js
+++ test/specs/destination.spec.js
@@ -115,6 +115,83 @@ describe("Destination", () => {
     expect(join(dir, "this/is/the/output/directory/sub/dir/file.txt")).to.be.a.file().with.contents("Helloooooooo");
   });
 
+  it('should clean the "dist" directory before building', async () => {
+    let dir = await createDir([
+      { path: "index.js", contents: "" },
+      { path: "src/file.txt", contents: "Hello, world!" },
+      { path: "src/subdir/index.html", contents: "<h1>Hello, World!</h1>" },
+      { path: "src/sub/dir/file.txt", contents: "Helloooooooo" },
+      { path: "dist/file.txt", contents: "OLD CONTENTS" },
+      { path: "dist/file2.txt", contents: "OLD CONTENTS" },
+      { path: "dist/subdir/index.html", contents: "OLD CONTENTS" },
+      { path: "dist/subdir/index2.html", contents: "OLD CONTENTS" },
+      { path: "dist/sub/dir/file.txt", contents: "OLD CONTENTS" },
+      { path: "dist/sub/dir/file2.txt", contents: "OLD CONTENTS" },
+    ]);
+
+    let process = new MockProcess(dir);
+    let cli = new CodeEngineCLI({ process });
+    await cli.main();
+
+    process.assert.stderr("");
+    process.assert.exitCode(0);
+
+    expect(join(dir, "dist")).to.be.a.directory().with.deep.contents([
+      "file.txt",
+      "sub",
+      "sub/dir",
+      "sub/dir/file.txt",
+      "subdir",
+      "subdir/index.html",
+    ]);
+
+    expect(join(dir, "dist/file.txt")).to.be.a.file().with.contents("Hello, world!");
+    expect(join(dir, "dist/subdir/index.html")).to.be.a.file().with.contents("<h1>Hello, World!</h1>");
+    expect(join(dir, "dist/sub/dir/file.txt")).to.be.a.file().with.contents("Helloooooooo");
+  });
+
+  it("should clean the destination directory before building", async () => {
+    let dir = await createDir([
+      {
+        path: "index.js",
+        contents: `
+          module.exports = {
+            destination: "output",
+          }
+        `
+      },
+      { path: "src/file.txt", contents: "Hello, world!" },
+      { path: "src/subdir/index.html", contents: "<h1>Hello, World!</h1>" },
+      { path: "src/sub/dir/file.txt", contents: "Helloooooooo" },
+      { path: "output/file.txt", contents: "OLD CONTENTS" },
+      { path: "output/file2.txt", contents: "OLD CONTENTS" },
+      { path: "output/subdir/index.html", contents: "OLD CONTENTS" },
+      { path: "output/subdir/index2.html", contents: "OLD CONTENTS" },
+      { path: "output/sub/dir/file.txt", contents: "OLD CONTENTS" },
+      { path: "output/sub/dir/file2.txt", contents: "OLD CONTENTS" },
+    ]);
+
+    let process = new MockProcess(dir);
+    let cli = new CodeEngineCLI({ process });
+    await cli.main();
+
+    process.assert.stderr("");
+    process.assert.exitCode(0);
+
+    expect(join(dir, "output")).to.be.a.directory().with.deep.contents([
+      "file.txt",
+      "sub",
+      "sub/dir",
+      "sub/dir/file.txt",
+      "subdir",
+      "subdir/index.html",
+    ]);
+
+    expect(join(dir, "output/file.txt")).to.be.a.file().with.contents("Hello, world!");
+    expect(join(dir, "output/subdir/index.html")).to.be.a.file().with.contents("<h1>Hello, World!</h1>");
+    expect(join(dir, "output/sub/dir/file.txt")).to.be.a.file().with.contents("Helloooooooo");
+  });
+
   it("should error if the destination is not a directory", async () => {
     let dir = await createDir([
       "index.js",
